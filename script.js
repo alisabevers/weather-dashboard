3de4049a2c68a7978c3fd396519179b7a670ae6b
A Bevers <alisabevers@gmail.com>
2023-04-15 15:55:35 -0400

    added day.js and created onclick event to save the user input into the search history

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-var APIkey = "124280980cdfebb4a20f890339a90071";
+var APIKey = "124280980cdfebb4a20f890339a90071";
+var city;
+var citySearchBtn = document.querySelector("#citySearchBtn");
+var citySearch = document.querySelector("#searchCity");
+var searchHistory = document.querySelector(".searchHistory");
+var date = dayjs();
+var todayWeatherHeader = document.querySelector("#today-weather-header");
 
+var todayTemp = document.querySelector("#todayTemp");
+var today
+
+// saves the user input / city name and adds to search history
+citySearchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (citySearch.value == "") {
+        return;
+    }
+    city = citySearch.value;
+    var newSearchHistory = document.createElement("button");
+    newSearchHistory.textContent = city.toUpperCase();
+    newSearchHistory.classList.add("btn");
+    newSearchHistory.classList.add("btn-primary");
+    searchHistory.appendChild(newSearchHistory);
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json()
+    .then(function (data) {
+        console.log(data);
+        todayWeatherHeader.textContent = data.name + " " + date.format('(M/D/YYYY)');
+        });
+    });
+});
